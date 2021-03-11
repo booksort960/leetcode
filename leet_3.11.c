871ff8cb52508385625af8617a87df97733a51aa
booksort960 <2141166187@qq.com>
2021-03-11 19:02:53 +0800

    #7，整数反转 #9，回文数 #13.罗马数字转整数

--- leet_3.11.c
+++ leet_3.11.c
@@ -68,3 +68,123 @@
 //		return 0;
 //	}
 //}
+//int rep(char n);
+//int romanToInt(char* s)
+//{
+//	int sum = 0;
+//
+//	int len = strlen(s);
+//	for (int i = 0; i < len - 1; i++)
+//	{
+//
+//		sum = (rep(s[i]) < rep(s[i + 1])) ? sum + rep(s[i + 1]) - rep(s[i]) : sum + rep(s[i]);
+//		if (i == len - 2)
+//			sum += rep(s[len - 1]);
+//	}
+//	return sum;
+//}
+//int rep(char n)
+//{
+//	switch (n)
+//	{
+//	case 'I':
+//		return 1;
+//	case 'V':
+//		return 5;
+//	case 'X':
+//		return 10;
+//	case 'L':
+//		return 50;
+//	case 'C':
+//		return 100;
+//	case 'D':
+//		return 500;
+//	case 'M':
+//		return 1000;
+//	default:
+//		return 0;
+//	}
+//}
+//int rep(char n)
+//{
+//	switch (n)
+//	{
+//	case 'I':
+//		return 1;
+//	case 'V':
+//		return 5;
+//	case 'X':
+//		return 10;
+//	case 'L':
+//		return 50;
+//	case 'C':
+//		return 100;
+//	case 'D':
+//		return 500;
+//	case 'M':
+//		return 1000;
+//	}
+//}
+//int romanToInt(char* s)
+//{
+//	int sum = 0;
+//
+//	int len = strlen(s);
+//	int pre=rep(s[0]);
+//	for (int i = 1; i < len ; i++)
+//	{
+//		if (pre<rep(s[i]))//出现5-1的情况
+//		{
+//			sum -= pre;
+//		}
+//		else
+//		{
+//			sum += rep(s[i]);
+//		}
+//		pre = rep(s[i]);
+//	}
+//	return sum;
+//}
+//int rep(char n);
+//int romanToInt(char* s)
+//{
+//	int sum = 0;
+//	int len = strlen(s);
+//	int pre = rep(s[0]);
+//	for (int i = 1; i < len; i++)
+//	{
+//		if (pre < rep(s[i]))//出现5-1的情况
+//		{
+//			sum -= pre;
+//		}
+//		else
+//		{
+//			sum += pre;
+//		}
+//		pre = rep(s[i]);
+//	}
+//	sum += pre;
+//	return sum;
+//}
+//int rep(char n)
+//{
+//	switch (n)
+//	{
+//	case 'I':
+//		return 1;
+//	case 'V':
+//		return 5;
+//	case 'X':
+//		return 10;
+//	case 'L':
+//		return 50;
+//	case 'C':
+//		return 100;
+//	case 'D':
+//		return 500;
+//	case 'M':
+//		return 1000;
+//	default:
+//		return 0;
+//	}
+//}
