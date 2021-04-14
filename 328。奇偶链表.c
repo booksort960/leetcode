8bcf82f0e7b351cc9b4df0167888e06d57863340
booksort960 <2141166187@qq.com>
2021-04-14 21:05:19 +0800

    328.奇偶链表（双哨兵尾插法）

--- 328。奇偶链表.c
+++ 328。奇偶链表.c
@@ -26,7 +26,3 @@ struct ListNode* oddEvenList(struct ListNode* head)
     free(evenhead);
     return p;
 }
-//用两个额外创建的结点一个负责尾插奇数位的结点，另一个负责尾插偶数位的结点。
-//同时用一个cur指针负责遍历单链表中的每个结点位。
-//cur访问为NULL后就将两个链表链接，合成一个链表。
-//同时要满足新链表的为结点->next要置空，否者会因为未改变其next的指向，造成问题可能是环形链表之类的。
