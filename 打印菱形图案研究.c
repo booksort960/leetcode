c7c5aab8625b01fae616ab1dc19ceb8074e34697
booksort960 <2141166187@qq.com>
2021-04-16 12:18:48 +0800

    打印菱形图案研究.c

---
+++ 打印菱形图案研究.c
@@ -0,0 +1,34 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//int main(void)
+//{
+//	int ROW, COL;
+//	scanf("%d", &ROW);
+//	COL = 2 * ROW - 1;
+//	for (int y = 0; y < 2 * ROW - 1; ++y)//上三角形
+//	{
+//		for (int x = 0; x < COL; ++x)
+//		{
+//			if ((y - x >= (1 - ROW) && y + x >= (ROW - 1)) && (y - x <= ROW - 1 && x + y <= 3 * (ROW - 1)))
+//				printf("*");
+//			else
+//				printf(" ");
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
+//	//for (int y = 0; y < ROW - 1; ++y)//下三角形
+//	//{
+//	//	for (int x = 0; x < COL - 1; ++x)
+//	//	{
+//	//		if ()
+//	//			printf("*");
+//	//		else
+//	//			printf(" ");
+//	//	}
+//	//	printf("\n");
+//	//}
+//	return 0;
+//
+//}
